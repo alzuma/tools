a974d95e7b29f99271c5111229ec166e7ebca534
Kristaps Vītoliņš <kristaps.vitolins@gmail.com>
2019-01-07 22:26:51 +0200

    1.0.11

--- dist/commands/AggregateRoot.d.ts
+++ dist/commands/AggregateRoot.d.ts
@@ -1,3 +1,7 @@
+import { TConsumer } from '../Types';
+import { Dictionary } from '..';
 export declare class AggregateRoot {
-    private handlers;
+    readonly handlers: Dictionary<string, TConsumer<object>>;
+    protected register: <T extends object>(c: new () => T, when: TConsumer<T>) => void;
+    protected raise: (item: object) => void;
 }
