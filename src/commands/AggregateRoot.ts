010b04fc9bfbfeee4ab92c02459f52eaa58bdd03
Kristaps Vītoliņš <kristaps.vitolins@gmail.com>
2019-01-07 22:26:13 +0200

    impl of aggregate root

--- src/commands/AggregateRoot.ts
+++ src/commands/AggregateRoot.ts
@@ -1,6 +1,17 @@
-import { TFunction } from '../Types';
+import { TConsumer } from '../Types';
 import { Dictionary } from '..';
 
 export class AggregateRoot {
-    private handlers: Dictionary<string, TFunction<object, Promise<void>>> = new Dictionary();
+    public readonly handlers: Dictionary<string, TConsumer<object>> = new Dictionary();
+
+    protected register = <T extends object>(c: new () => T, when: TConsumer<T>) => {
+        this.handlers.add(c.name, w => when(w as T));
+    };
+
+    protected raise = (item: object) => {
+        const consumer = this.handlers.get(item.constructor.name);
+        if (consumer) {
+            consumer(item);
+        }
+    };
 }
