a2ba397b666f221e1e2ea594d5ddcd5bfc8862cb
Kristaps Vītoliņš <kristaps.vitolins@gmail.com>
2020-10-14 20:55:35 +0300

    Update Types.ts
    Add Action type

--- src/Types.ts
+++ src/Types.ts
@@ -2,6 +2,7 @@ export type Func<T, R> = (t: T) => R;
 export type BiFunc<T, U, R> = (t: T, u: U) => R;
 export type Supplier<T> = () => T;
 export type Consumer<T> = (accept: T) => void;
+export type Action = () => void;
 
 export interface IIterable<T> {
     iterate: (accept: Consumer<T>) => void;
